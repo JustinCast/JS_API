7582618585d5332108e05995f2777b4e29f0246a
JustinCast <justincastillovalladares@gmail.com>
2019-11-16 17:02:45 -0600

    deploy: last changes to deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { AuthGuardService } from './services/auth-guard.service';
 const routes: Routes = [
   { path: "dashboard", component: DashboardComponent , canActivate: [AuthGuardService] },
   { path: "home", component: HomeComponent },
-  { path: "functions", component: FunctionComponent },
+  { path: "fns", component: FunctionComponent },
   { path: '',
     redirectTo: '/home',
     pathMatch: 'full'
