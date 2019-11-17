1445e3361665eb7a581a15e6e5df2fd9d58817a8
JustinCast <justincastillovalladares@gmail.com>
2019-11-17 14:45:55 -0600

    feat: last changes to deploy

--- dist/js-api/main.js
+++ dist/js-api/main.js
@@ -1749,7 +1749,7 @@ var ɵ0 = function () {
 };
 exports.ɵ0 = ɵ0;
 var monacoConfig = {
-    baseUrl: "/js-api/assets",
+    baseUrl: "assets",
     defaultOptions: { scrollBeyondLastLine: false },
     onMonacoLoad: ɵ0,
 };
--- src/app/monaco-config.ts
+++ src/app/monaco-config.ts
@@ -1,7 +1,7 @@
 import { NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 const monacoConfig: NgxMonacoEditorConfig = {
-  baseUrl: "/js-api/assets",
+  baseUrl: "assets",
   defaultOptions: { scrollBeyondLastLine: false }, // pass default options to be used
   onMonacoLoad: function()  {
     console.log((<any>window).monaco);
