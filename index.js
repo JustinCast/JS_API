39e512e57db26008a0f05f26867f127e39987801
JustinCast <justincastillovalladares@gmail.com>
2019-11-16 18:19:41 -0600

    deploy: last changes to deploy

--- index.js
+++ index.js
@@ -7,42 +7,50 @@ var FunctionRouter_1 = require("./dist-server/src/routes/FunctionRouter");
 var UserRouter_1 = require("./dist-server/src/routes/UserRouter");
 var DinamicApi_1 = require("./dist-server/src/routes/DinamicApi");
 var config = require("dotenv").config;
-var Server = /** @class */ (function () {
-    function Server() {
-        this.app = express();
-        config();
-        this.config();
-        this.routerConfig();
-    }
-    Server.prototype.config = function () {
-        // body-parser parsea el contenido proveniente en la solicitud
-        // para permitir una interface de tratamiento de datos más fácil
-        this.app.use(body_parser_1.urlencoded({ extended: true }));
-        this.app.use(body_parser_1.json());
-        this.app.use(function (_req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            res.setHeader("Content-Type", "application/json");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-            res.header("Access-Control-Allow-Credentials", "true");
-            next(); // chain the another middleware in pipeline
-        });
-    };
-    /**
-     * Config of routes
-     */
-    Server.prototype.routerConfig = function () {
-        //this.app.use(express.static(__dirname + "/dist/js-api"));
-        this.app.use("/functions", FunctionRouter_1["default"]);
-        this.app.use("/users", UserRouter_1["default"]);
-        this.app.use("/dinamicAPI", DinamicApi_1["default"]);
-        this.app.get("/*", function (req, res) {
-            res.sendFile(path.join("./dist/js-api/index.html"));
-        });
-        //Set Port
-        this.app.listen(5000);
-        console.log("Server running on port 5000")
-    };
-    return Server;
-}());
+var Server = /** @class */ (function() {
+  function Server() {
+    this.app = express();
+    config();
+    this.config();
+    this.routerConfig();
+  }
+  Server.prototype.config = function() {
+    // body-parser parsea el contenido proveniente en la solicitud
+    // para permitir una interface de tratamiento de datos más fácil
+    this.app.use(body_parser_1.urlencoded({ extended: true }));
+    this.app.use(body_parser_1.json());
+    this.app.use(function(_req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+      );
+      res.setHeader("Content-Type", "application/json");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+      );
+      res.header("Access-Control-Allow-Credentials", "true");
+      next(); // chain the another middleware in pipeline
+    });
+  };
+  /**
+   * Config of routes
+   */
+  Server.prototype.routerConfig = function() {
+    app.use("/functions", FunctionRouter_1["default"]);
+    app.use("/users", UserRouter_1["default"]);
+    app.use("/dinamicAPI", DinamicApi_1["default"]);
+    app.use(express.static(__dirname + "/dist/js-api"));
+    app.get("/*", function(req, res) {
+      res.sendFile(
+        path.join(__dirname + "/dist/js-api/index.html")
+      );
+    });
+    //Set Port
+    this.app.listen(5000);
+    console.log("Server running on port 5000");
+  };
+  return Server;
+})();
 exports["default"] = new Server().app;
