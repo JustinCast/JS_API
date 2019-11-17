2fa188f5975d813a1620c1d62b1c8ee6c77cdd0c
JustinCast <justincastillovalladares@gmail.com>
2019-11-17 14:42:54 -0600

    feat: last changes to deploy

--- src/app/monaco-config.ts
+++ src/app/monaco-config.ts
@@ -1,9 +1,9 @@
 import { NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 const monacoConfig: NgxMonacoEditorConfig = {
-  baseUrl: "/assets",
+  baseUrl: "/js-api/assets",
   defaultOptions: { scrollBeyondLastLine: false }, // pass default options to be used
-  onMonacoLoad: function() {
+  onMonacoLoad: function()  {
     console.log((<any>window).monaco);
   }, // here monaco object will be available as window.monaco use this function to extend monaco editor functionalities.
 };
