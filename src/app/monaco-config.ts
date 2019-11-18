42178d525f98ce320aee2dd01e53d60f1d91ab3a
JustinCast <justincastillovalladares@gmail.com>
2019-11-18 11:15:44 -0600

    testing new build

--- src/app/monaco-config.ts
+++ src/app/monaco-config.ts
@@ -1,7 +1,7 @@
 import { NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 const monacoConfig: NgxMonacoEditorConfig = {
-  baseUrl: "/js-api/assets", // pass default options to be used
+  baseUrl: "js-api/assets", // pass default options to be used
   onMonacoLoad: function()  {
     console.log((<any>window).monaco);
   }, // here monaco object will be available as window.monaco use this function to extend monaco editor functionalities.
